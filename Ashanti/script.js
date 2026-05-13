60db39a9f12feb6c5a2b0c93eb95f424e16ecd4e
Ashanti <ashantilemonia@gmail.com>
2026-05-13 14:05:13 -0500

    added updates

--- Ashanti/script.js
+++ Ashanti/script.js
@@ -1,4 +1,6 @@
 //console.log("Hello world!");
+const pTag = document.getElementById("attempt1").innerText = "Hey Yall!";
+
 
 const dresses = ["formal", "ball gown", "casual", "church", "work"];
 const dress = dresses[3];
